973d31f698f2b5551f4784d9f864d675b30ce0e5
hailinzparra <hailinzparra@gmail.com>
2023-05-03 21:41:47 +0700

    drawing bg

--- ts/base/scenes/playground.ts
+++ ts/base/scenes/playground.ts
@@ -1,6 +1,7 @@
 interface ScenePlaygroundProps {
     ekg_box_h: number
     ekg_box_small_box_size: number
+    ekg_box_layout_ymargin: number
     ekg_box_main: EKGBox
     ekg_box_1: EKGBox
     ekg_box_2: EKGBox
@@ -14,14 +15,20 @@ interface ScenePlaygroundProps {
     ekg_box_v4: EKGBox
     ekg_box_v5: EKGBox
     ekg_box_v6: EKGBox
+    init_ekg_box(): void
     point_inside_ekg_box(p: CoreVec2, ekg_box: EKGBox): boolean
     main_box_selection_logic(): void
-    main_box_selection_render(): void
+    main_box_selection_render_ui(): void
+
+    drawing_area_box_size: number
+    render_drawing_area(): void
+    render(): void
 }
 
 const scene_playground = new CoreScene<ScenePlaygroundProps>('Playground', {
     ekg_box_h: 120,
     ekg_box_small_box_size: 4,
+    ekg_box_layout_ymargin: 8,
     ekg_box_main: new EKGBox(''),
     ekg_box_1: new EKGBox('I'),
     ekg_box_2: new EKGBox('II'),
@@ -35,6 +42,44 @@ const scene_playground = new CoreScene<ScenePlaygroundProps>('Playground', {
     ekg_box_v4: new EKGBox('V4'),
     ekg_box_v5: new EKGBox('V5'),
     ekg_box_v6: new EKGBox('V6'),
+    init_ekg_box() {
+        const s = this.ekg_box_small_box_size
+        const w = Math.floor(stage.size.x / 4 / (s * 5)) * (s * 5)
+        const h = this.ekg_box_h
+        let x = (stage.size.x - w * 4) / 2
+        let y = stage.size.y - h / 2 - this.ekg_box_layout_ymargin
+        this.ekg_box_3.init(x, y, w, h, s)
+        this.ekg_box_avf.init(x + w, y, w, h, s)
+        this.ekg_box_v3.init(x + 2 * w, y, w, h, s)
+        this.ekg_box_v6.init(x + 3 * w, y, w, h, s)
+        y -= h
+        this.ekg_box_2.init(x, y, w, h, s)
+        this.ekg_box_avl.init(x + w, y, w, h, s)
+        this.ekg_box_v2.init(x + 2 * w, y, w, h, s)
+        this.ekg_box_v5.init(x + 3 * w, y, w, h, s)
+        y -= h
+        this.ekg_box_1.init(x, y, w, h, s)
+        this.ekg_box_avr.init(x + w, y, w, h, s)
+        this.ekg_box_v1.init(x + 2 * w, y, w, h, s)
+        this.ekg_box_v4.init(x + 3 * w, y, w, h, s)
+        y -= h
+        this.ekg_box_main.init(x, y, w * 4, h, s)
+        this.ekg_box_main.lead = this.ekg_box_2.lead
+        this.ekg_box_main.name = this.ekg_box_2.name
+        obj.instantiate('ekg_box', this.ekg_box_main)
+        obj.instantiate('ekg_box', this.ekg_box_1)
+        obj.instantiate('ekg_box', this.ekg_box_2)
+        obj.instantiate('ekg_box', this.ekg_box_3)
+        obj.instantiate('ekg_box', this.ekg_box_avr)
+        obj.instantiate('ekg_box', this.ekg_box_avl)
+        obj.instantiate('ekg_box', this.ekg_box_avf)
+        obj.instantiate('ekg_box', this.ekg_box_v1)
+        obj.instantiate('ekg_box', this.ekg_box_v2)
+        obj.instantiate('ekg_box', this.ekg_box_v3)
+        obj.instantiate('ekg_box', this.ekg_box_v4)
+        obj.instantiate('ekg_box', this.ekg_box_v5)
+        obj.instantiate('ekg_box', this.ekg_box_v6)
+    },
     point_inside_ekg_box(p, box) {
         return p.x > box.x && p.x < box.x + box.width
             && p.y > box.y - box.height / 2 && p.y < box.y + box.height / 2
@@ -50,7 +95,7 @@ const scene_playground = new CoreScene<ScenePlaygroundProps>('Playground', {
             }
         }
     },
-    main_box_selection_render() {
+    main_box_selection_render_ui() {
         draw.set_color('#000')
         const main = this.ekg_box_main
         for (const box of obj.take<EKGBox>('ekg_box')) {
@@ -60,46 +105,39 @@ const scene_playground = new CoreScene<ScenePlaygroundProps>('Playground', {
             }
         }
     },
+
+    drawing_area_box_size: 64,
+    render_drawing_area() {
+        const h = stage.size.y - this.ekg_box_h * 4 - this.ekg_box_layout_ymargin * 2
+        const s = this.drawing_area_box_size
+        const tile_xamount = Math.ceil(stage.size.x / s)
+        const tile_yamount = Math.ceil(h / s)
+        const tile_xoffset = (stage.size.x - tile_xamount * s) / 2
+        const tile_yoffset = (h - tile_yamount * s) / 2
+
+        draw.set_color('#34027a')
+        draw.rect(0, 0, stage.size.x, h)
+
+        draw.set_color('#3823a3')
+        draw.ctx.lineWidth = 4
+        for (let x = 0; x < tile_xamount; x++) {
+            draw.line(tile_xoffset + x * s, 0, tile_xoffset + x * s, h)
+        }
+        for (let y = 0; y < tile_yamount; y++) {
+            draw.line(0, tile_yoffset + y * s, stage.size.x, tile_yoffset + y * s)
+        }
+        draw.ctx.lineWidth = 1
+
+        // border bottom
+        draw.line(0, h, stage.size.x, h)
+    },
+    render() {
+        this.render_drawing_area()
+    },
 })
 
 scene_playground.start = () => {
-    const p = scene_playground.props
-    const s = p.ekg_box_small_box_size
-    const w = Math.floor(stage.size.x / 4 / (s * 5)) * (s * 5)
-    const h = p.ekg_box_h
-    let x = (stage.size.x - w * 4) / 2
-    let y = stage.size.y - h / 2
-    p.ekg_box_3.init(x, y, w, h, s)
-    p.ekg_box_avf.init(x + w, y, w, h, s)
-    p.ekg_box_v3.init(x + 2 * w, y, w, h, s)
-    p.ekg_box_v6.init(x + 3 * w, y, w, h, s)
-    y -= h
-    p.ekg_box_2.init(x, y, w, h, s)
-    p.ekg_box_avl.init(x + w, y, w, h, s)
-    p.ekg_box_v2.init(x + 2 * w, y, w, h, s)
-    p.ekg_box_v5.init(x + 3 * w, y, w, h, s)
-    y -= h
-    p.ekg_box_1.init(x, y, w, h, s)
-    p.ekg_box_avr.init(x + w, y, w, h, s)
-    p.ekg_box_v1.init(x + 2 * w, y, w, h, s)
-    p.ekg_box_v4.init(x + 3 * w, y, w, h, s)
-    y -= h
-    p.ekg_box_main.init(x, y, w * 4, h, s)
-    p.ekg_box_main.lead = p.ekg_box_2.lead
-    p.ekg_box_main.name = p.ekg_box_2.name
-    obj.instantiate('ekg_box', p.ekg_box_main)
-    obj.instantiate('ekg_box', p.ekg_box_1)
-    obj.instantiate('ekg_box', p.ekg_box_2)
-    obj.instantiate('ekg_box', p.ekg_box_3)
-    obj.instantiate('ekg_box', p.ekg_box_avr)
-    obj.instantiate('ekg_box', p.ekg_box_avl)
-    obj.instantiate('ekg_box', p.ekg_box_avf)
-    obj.instantiate('ekg_box', p.ekg_box_v1)
-    obj.instantiate('ekg_box', p.ekg_box_v2)
-    obj.instantiate('ekg_box', p.ekg_box_v3)
-    obj.instantiate('ekg_box', p.ekg_box_v4)
-    obj.instantiate('ekg_box', p.ekg_box_v5)
-    obj.instantiate('ekg_box', p.ekg_box_v6)
+    scene_playground.props.init_ekg_box()
 }
 
 scene_playground.update = () => {
@@ -107,8 +145,11 @@ scene_playground.update = () => {
 }
 
 scene_playground.render = () => {
+    scene_playground.props.render()
 }
 
 scene_playground.render_ui = () => {
-    scene_playground.props.main_box_selection_render()
+    scene_playground.props.main_box_selection_render_ui()
+
+    debug.draw_fps(0, 0)
 }
--- ts/core/debug.ts
+++ ts/core/debug.ts
@@ -1,12 +1,31 @@
 interface CoreDebug {
     debug_index: number
     odd(): boolean
+    draw_fps(x: number, y: number): void
 }
 
 core.debug = {
     debug_index: 0,
     odd() {
         return this.debug_index % 2 !== 0
+    },
+    draw_fps(x, y) {
+        const t = `${Math.round(core.time.fps)}`
+
+        core.draw.set_font(font.s)
+        const tw = core.draw.get_text_width(t)
+        const th = core.draw.get_text_height(t)
+
+        core.draw.set_alpha(0.5)
+
+        core.draw.set_color('#000')
+        core.draw.rect(0, 0, tw, th)
+
+        core.draw.set_hvalign('left', 'top')
+        core.draw.set_color('#fff')
+        core.draw.text(x, y, t)
+
+        core.draw.reset_alpha()
     },
 }
 
